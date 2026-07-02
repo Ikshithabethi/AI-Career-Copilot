f02c3738a2d6a2ac2b3688b02af5bdb64b68f942
Bethi Ikshitha <bethiikshitha@Bethis-MacBook-Air.local>
2026-07-02 18:39:34 +0530

    Complete authentication module with JWT

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
-
+const verifyToken = require("./middleware/authMiddleware");
 const app = express();
 
 app.use(cors());
@@ -14,7 +14,14 @@ console.log(authRoutes);
 app.get("/", (req, res) => {
     res.send("AI Career Copilot Backend Running");
 });
+app.get("/api/dashboard", verifyToken, (req, res) => {
 
+    res.json({
+        message: "Welcome to AI Career Copilot Dashboard",
+        user: req.user
+    });
+
+});
 const PORT = process.env.PORT || 5001;
 app.use("/api/auth", authRoutes);
 
